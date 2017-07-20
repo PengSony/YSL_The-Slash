c407cedff079db97c04020b4bd2412c0ffa8a1ae
PengSony <pengsony92@gmail.com>
2017-07-20 15:52:08 +0700

    Update On YSL_The Slash

---
+++ YSL_theSlash/resources/views/welcome.blade.php
@@ -0,0 +1,262 @@
+<html>
+<head>
+    <title itemprop='name'>Landing Page | Yellow Sea Logistic | Cambodia</title>
+    <link href="http://yellowlg.demo.flexitech.io/img/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1.0, user-scalable=no">
+    <link href="https://fonts.google.com/specimen/Roboto+Condensed?selection.family=Roboto+Condensed" rel="stylesheet" type="text/css">
+    <link href="{{asset('bootstrap-3.3.7-dist/css/bootstrap.min.css')}}" type="text/css" rel="stylesheet">
+    <link href="{{asset('css/styleWelcome.css')}}" type="text/css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="{{asset('bootstrap-3.3.7-dist/js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('bootstrap-3.3.7-dist/js/jquery-3.2.1.js')}}"></script>
+
+</head>
+<body>
+
+    <div class="Header_NavBar">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                <div class="Main_logo" >
+                    <img src="image/yellow-sea-logo.png" style="height: 65px;width: 30%;vertical-align: middle;box-sizing: inherit">
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                <div class="NavigationBar" style="height: 100px">
+                    <div class="Navbar">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <div class="navbar-collapse collapse">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#">HOME</a> </li>
+                                <li><a href="#">About US</a></li>
+                                <li><a href="#">Products&Service</a></li>
+                                <li><a href="#">NEWS</a> </li>
+                                <li><a href="#">Quote Request</a></li>
+                                <li><a href="#">Contact US</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    <div class="slider">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+            <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active" style="z-index: 10;width: 6%;height: 1px"></li>
+            <li data-target="#myCarousel" data-slide-to="1" style="z-index: 15;width: 6%;height: 1px"></li>
+            <li data-target="#myCarousel" data-slide-to="2" style="z-index: 15;width: 6%;height: 1px"></li>
+            <li data-target="#myCarousel" data-slide-to="3" style="z-index: 15;width: 6%;height: 1px"></li>
+            <li data-target="#myCarousel" data-slide-to="4" style="z-index: 15;width: 6%;height: 1px"></li>
+            </ol>
+             <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="image/port-logistic-2.jpg"style="width:100%;height: 600px;">
+                </div>
+                <div class="item">
+                    <img src="image/y-about-2.jpg" style="width:100%; height: 600px;">
+                </div>
+
+                <div class="item">
+                    <img src="image/new-slide-3.jpg"  style="width:100%;height: 600px;">
+                </div>
+                <div class="item">
+                    <img src="image/air-logistic.jpg"  style="width:100%;height: 600px;">
+                </div>
+                <div class="item">
+                    <img src="image/truck-logistic.jpg"  style="width:100%;height: 600px;">
+                </div>
+            </div>
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <div class="Text">
+            <p>YELLOW SEA LOGISTICS offers the supports for the foods transportation both
+                <br>locally and globally such as Rail Freight, Intermodal, Air Freight, Ocean Freight,
+                <br>Custom Broker and Warehousing /Distribution
+            </p>
+            <button class="ButtonHead">QUOTE REQUEST</button>
+        </div>
+    </div>
+    <div class="feature">
+        <div class="jumbotron">
+            <div class="container-fluid" style="max-width:1400px;">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p>About <span style="color: #f9c803">Yellowsea Logistic</span></p>
+                        <p>Yellow Sea Logistic offers the supports for the goods transportation both locally and globally such as Rail Freight,
+                           Intermodal,Air Freight,Ocean Freight,Custom Broker,and Warehousing/ Distribution.<br>
+                        </p>
+                        <p>Based on our strong assets and facilities,we have been motivated to start our first service providing in 2016,
+                           and then with many more services from the start to the end.We would like our customer to rest assured and let us take care of the rest for you.
+                           We are new in term of company, but we have a talented and experienced team here to ensure a high quality service being delivered and satisfied
+                           to our customers. <br/>
+                        </p>
+                        <p>We are doing our best as a responsible business partner and a customer focused service to bring value and solution to your business
+                           develoment as well as ours.
+                        </p>
+                    </div>
+                </div>
+                <div class="Image">
+                    <img src="../image/port-logistic.jpg" style="width:100%; height:500px;">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="Body">
+        <div class="txt_productService">
+            <h2>Product &amp; Services</h2><hr>
+        </div>
+        <div class="container" style="max-width: 1400px;">
+            <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                    <div class="intermodalService">
+                        <h2>Intermodal Service</h2>
+                        <p>Our core product is to provide you the stable but qualified delivery service.
+                            We have a station where you can easily drop your cargo and leave everything to us.<br>On top of that,We have the intermodal
+                            service to support you an all-in delivery to your warehouse directly.
+                        </p>
+
+                        <p>We have a strong product designed as we have a warehouse combined with local<br>
+                            intermodal service. It will save you time, cost and security.Your cargos will delivered at the destination all at the same time.
+                        </p>
+                        <hr style="border: 1px solid yellow; width: 80px; text-align: center">
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="img_intermodal">
+                        <img src="../image/intermodal_main.jpg" class="img-responsive" style="width:100%; height:300px;">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8 col-md-push-4 col-sm-12 col-xs-12">
+                    <div class="txt_freigh">
+                        <h2>Freight Forwarding</h2>
+                        <p>By Ocean or Air, FCL or LCL,we believe that we are the one for you.We focus to offer the fast
+                            and premium service/Port to Port/ to Port/Port to Door.</p>
+                        <p>We desing our service as per your individual requirements from/to almost anywhere
+                            you wish to ship your cargo in the globe.We handle them with <br>Care and Responsibility.
+                            Given your available time range, we will support you to present your cargos to anypalce
+                            of your request with the cost effectiveness.</p>
+                        <hr>
+                    </div>
+                </div>
+                <div class="col-md-4 col-md-pull-8 col-sm-12 col-xs-12">
+                    <div class="img_freign">
+                        <img src="../image/frieght_forward.jpg" class="img-responsive" style="width:100%; height:300px;">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                    <div class="txt_custom">
+                        <h2>Custom House Brokerags</h2>
+                        <p>For ease of doing your trading business,we urge to provide you -Custom
+                            House Brokerage service supporting your imports businesses.<br>
+                        </p>
+                        <p>We will cover all the praparations of required documents, taxes/duties caculation and
+                           payment, ensuring customs fomality completion in the fast maner, so your cargo will be available as planned
+                        </p>
+                        <p>We have a strong product designed as we have a warehouse combined with local<br>
+                           intermodal service. It will save you time, cost and security.Your cargos will delivered at the destination all at the same time.
+                        </p>
+                        <hr>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="img_custom">
+                        <img src="../image/custom-brokerage.jpg" class="img-responsive" style="width:100%; height:300px;">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8 col-md-push-4 col-sm-12 col-xs-12">
+                    <div class="txt_warehousing">
+                        <h2>WareHousing and Distribution</h2>
+                        <p>Storage is crucially important for your cargos as it is the starting before
+                            bringing your cargos to the world or to your retails shop.<br></p>
+                        <p>We keep it a priority when it comes to this. That is why we have our own
+                            warehouse which is standardize, highly maintianed and ideally safe. There is also our value
+                            added service to distribute your wish-list cargo to you whether it is in a small or big volume.
+                        </p>
+                        <hr>
+                    </div>
+                </div>
+                <div class="col-md-4 col-md-pull-8 col-sm-12 col-xs-12">
+                    <div class="img_warehousing">
+                        <img src="../image/new-warehousing.jpg" class="img-responsive" style="width:100%; height:300px;">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+            <div class="Footer">
+                <div class="row">
+                    <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                        <div class="img_near_footer">
+                            <div class="contain"></div>
+                            <div class="txt_near_footer">
+                                <p>YELLOW SEA LOGISTICS offers the supports for the foods transportation both<br>
+                                    locally and globally such as Rail Freight, Intermodal
+                                    ,Air Freight, Ocean Freight,<br>Custom Broker and Warehousing /Distribution
+                                </p>
+                            </div>
+                            <div class="footer_btn">
+                                <button>Quote Request</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12 visible-lg visible-md visible-sm visible-xs text-center">
+                        <div class="footer_text">
+                            <p>FOLLOW US</p>
+                        </div>
+                        <div class="Social_Item">
+                            <!-- Add font awesome icons -->
+                            <a href="https://www.facebook.com/" class="fa fa-facebook-square" style="font-size: 40px"></a>
+                            <a href="#" class="fa fa-twitter" style="font-size: 40px"></a>
+                            <a href="#" class="fa fa-instagram" style="font-size: 40px"></a>
+                            <a href="#" class="fa fa-Google-Plus" style="font-size: 40px"></a>
+                            <a href="#" class="fa fa-youtube-play" style="font-size: 40px"></a>
+                        </div>
+                    </div>
+                    <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12 visible-lg visible-md visible-sm visible-xs text-center">
+                        <div class="End_footer">
+                            <div class="NavbarFooter">
+                                <ul>
+                                <li><a href="#">About US</a></li>
+                                <li><a href="#">Products & Service</a></li>
+                                <li><a href="#">Quote Request</a></li>
+                                <li><a href="#">Contact US</a></li>
+                                </ul>
+                            </div>
+                            <hr>
+                            <div class="copyRight">
+                                <p>&copy; 2017 Yellow Sea Logistic. All right reserved.Designed by <strong>Flexitech</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+</body>
+</html>
