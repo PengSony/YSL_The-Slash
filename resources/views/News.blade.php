76d3bc0b0c71c2c39c1e8441844bd556c6b58aa2
PengSony <pengsony92@gmail.com>
2017-08-22 09:22:56 +0700

    Updated

---
+++ resources/views/News.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pengsony
+ * Date: 8/15/2017
+ * Time: 2:57 PM
+ */
---
+++ resources/views/QuoteRequest.blade.php
@@ -0,0 +1,27 @@
+@extends('welcome')
+@section('content')
+
+
+
+
+
+
+    <h1 style="font-size: 100px">QuoteRequest</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
